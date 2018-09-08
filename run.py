02d431049e75cd075fa086ae038cee0adc60726e
yyy2015 <2456700983@qq.com>
2018-09-08 10:40:37 +0800

    upload

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask_restplus import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restplus import Resource, Api, reqparse, fields
 from flask_mongoengine import MongoEngine
-
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -15,15 +16,24 @@ db = MongoEngine(app)
 api = Api(app)
 
 
-class Indicator(db.Document):
+class Entry(db.Document):
+    country = db.StringField()
+    date = db.StringField()
+    value = db.DecimalField()
+
+
+class IndicatorCollection(db.Document):
     meta = {
-        'collection': 'todo',
+        'collection': 'indicator_collection',
         'ordering': ['-create_at'],
         'strict': False,
     }
 
-    task = db.StringField()
-    is_completed = db.BooleanField(default=False)
+    collection_id = db.SequenceField()
+    indicator = db.StringField()
+    indicator_value = db.StringField()
+    creation_time = db.DateTimeField(default=datetime.now)
+    entries = db.ListField(db.EmbeddedDocumentField('Entry'))
 
     def to_dict(self):
         return {
@@ -34,22 +44,45 @@ class Indicator(db.Document):
         }
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
+id_parser = reqparse.RequestParser()
+id_parser.add_argument('indicator_id', type=str, required=True, help='Indicator id')
+
+collection_model = api.model('Model', {
+    'location': fields.String,
+    'collection_id': fields.String,
+    'creation_time': fields.DateTime(dt_format='ISO 8601'),
+    'indicator': {
+        'id': fields.String,
+        'value': fields.String
+    }
+})
+
+
+@api.route('/collections')
+class IndicatorCollectionController(Resource):
     def get(self):
-        devices = Indicator.objects().all()
-        if not devices:
-            return "<p>No todos exist! <a href='/addtodo'>Add todo first.</a></p>"
         return {'hello': 'world'}
 
     def post(self):
-        todo1 = Indicator(task='task 1', is_completed=False)
-        todo2 = Indicator(task='task 2', is_completed=False)
-        todo3 = Indicator(task='task 3', is_completed=False)
-        todo1.save()
-        todo2.save()
-        todo3.save()
-        return "<p>add succssfully! <a href='/'>Home</a></p>"
+        args = id_parser.parse_args()
+        indicator_id = request.form['indicator_id']
+        url = "http://api.worldbank.org/v2/countries/all/indicators/" + indicator_id + "?date=2012:2017&format=json"
+        response = requests.get(url)
+
+        data = response.json()
+        ic = IndicatorCollection()
+        print(data[1][0])
+        ic.indicator = data[1][0].indicator.id
+        ic.indicator_value = data[1][0].indicator.value
+
+        for item in data[1]:
+            entry = Entry()
+            entry.country = item.country.value
+            entry.date = item.date
+            entry.value = item.value
+            ic.entries.append(entry)
+
+        return ic.save()
 
 
 if __name__ == '__main__':
